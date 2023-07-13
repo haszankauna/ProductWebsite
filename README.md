331f427ce28a3fe7e46f5fdc3b40656f74f47beb
haszanakauna <haszankauna@gmail.com>
2023-07-13 06:22:39 +0100

    added the AI picker

---
+++ README.md
@@ -0,0 +1,22 @@
+# Building and Deploying an AI-Powered 3D Website in React 
+![3D Website](https://i.ibb.co/Krk39Cf/Thumbnali.png)
+
+## Introduction
+We have seen over a couple of years how integrating 3D animations into websites makes the website user friendly and importantly gives the user an experience. An example is the apple website in which every user has an experience navigating the webpage. Which makes it part of the experience.
+
+## Technologies and concept used in this project include:
+- ThreeJS - A powerful 3D graphics library for rendering and animating the 3D model
+- React Three Fiber - A library for creating 3D graphics with ThreeJS in React
+- TailwindCSS - A CSS styling framework
+- Framer Motion - For animations
+
+## Core concepts used: 
+- Loading, creating and customizing stunning 3D models and geometries with various lights, while also understanding the 3D world with a camera and the positioning of an object in space.
+- Making the code reusable and scalable using Higher Order Components (HOCs) and industry-standard best practices
+- Adding custom color and file support.
+- Generating and using images through DALLE AI
+- Being able to download the resulting t-shirt model image
+- Ensuring responsiveness across all devices and improve your site's performance
+
+## Conclusion:
+A webpage should give users an experience while navigating the webpage. This makes users feel valued and have a keen interest for the webpage. It is also important to note that animations should be done with the users in mind while also noting the experience that comes with it. 
--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { easing  } from 'maath';
+import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
@@ -7,20 +7,18 @@ import { Decal, useGLTF, useTexture } from '@react-three/drei';
 import state from '../store';
 
 function Shirt() {
-const snap = useSnapshot(state);
-const { nodes, materials } = useGLTF('shirt_baked.glb')
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
 
-const logoTexture = useTexture(snap.logoDecal);
-const fullTexture = useTexture(snap.fullDecal);
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
 
-useFrame((state, delta) => easing.dampC(materials.lambert1.color,
-snap.color, 0.25, delta));
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
 
-const stateString = JSON.stringify(snap);
+  const stateString = JSON.stringify(snap);
 
   return (
-    <group
-       key={stateString}>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
@@ -29,24 +27,26 @@ const stateString = JSON.stringify(snap);
         dispose={null}
       >
         {snap.isFullTexture && (
-          <Decal
+          <Decal 
             position={[0, 0, 0]}
             rotation={[0, 0, 0]}
             scale={1}
-            map={fullTexture} />
+            map={fullTexture}
+          />
         )}
 
-         {snap.isLogoTexture && (
-          <Decal
+        {snap.isLogoTexture && (
+          <Decal 
             position={[0, 0.04, 0.15]}
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            map-antisotropy={16}
+            map-anisotropy={16}
             depthTest={false}
-            depthWrite={true} />
+            depthWrite={true}
+          />
         )}
-        </mesh>
+      </mesh>
     </group>
   )
 }
